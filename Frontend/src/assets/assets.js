a42d4a1f5bcba79efbc00bd415b732ca60cf19ff
Njosephine <josephinenanyonga389@gmail.com>
2024-11-11 14:51:25 +0300

    frontend

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,18 @@
+import dashboard_icon from './dashboard-icon.png'
+import inventory_icon from './inventory-icon.png'
+import supplier_icon from './supplier-icon.png'
+import order_icon from './order-icon.png'
+import sale from './sale.png'
+import logo from './logo.png'
+import inventory from './inventory.png'
+import purchase from './purchase.png'
+export const assets = {
+    dashboard_icon,
+    inventory_icon,
+    supplier_icon,
+    order_icon,
+    sale,
+    purchase,
+    logo,
+    inventory
+}
