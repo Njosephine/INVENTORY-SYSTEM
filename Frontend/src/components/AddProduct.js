17894b6b6b9ca44416b180357dd4bf9ae9e96fe8
Njosephine <josephinenanyonga389@gmail.com>
2024-11-13 14:37:42 +0300

    image field

--- Frontend/src/components/AddProduct.js
+++ Frontend/src/components/AddProduct.js
@@ -13,6 +13,7 @@ export default function AddProduct({
     name: "",
     manufacturer: "",
     description: "",
+    imageUrl: "",
   });
   console.log("----",product)
   const [open, setOpen] = useState(true);
@@ -124,6 +125,26 @@ export default function AddProduct({
                               }
                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                               placeholder="Ex. Apple"
+                            />
+                          </div>
+
+                          <div>
+                            <label
+                              for="price"
+                              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                              Image
+                            </label>
+                            <input
+                              type="image"
+                              name="image"
+                              id="image"
+                              value={product.imageUrl}
+                              onChange={(e) =>
+                                handleInputChange(e.target.name, e.target.value)
+                              }
+                              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                              placeholder="$299"
                             />
                           </div>
                           {/* <div>
